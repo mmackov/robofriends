03c02074ebb39e0be68e1ebae45d0dadc7cc44ee
mmackov <martin.mackov@outlook.com>
2020-07-11 20:29:02 +0200

    Add ability to switch robots source between API and LOCAL mode

--- src/actions.js
+++ src/actions.js
@@ -1,19 +1,32 @@
 import {
 	CHANGE_SEARCH_FIELD,
+	SWITCH_SOURCE,
+	SOURCE_API,
+	SOURCE_LOCAL,
 	REQUEST_ROBOTS_PENDING,
 	REQUEST_ROBOTS_SUCCESS,
 	REQUEST_ROBOTS_FAILED
 } from './constants.js';
+
+import { robots } from './robots.js';
 
 export const setSearchField = (text) => ({
 	type: CHANGE_SEARCH_FIELD,
 	payload: text
 })
 
-export const requestRobots = () => (dispatch) => {
+export const switchSource = () => ({
+	type: SWITCH_SOURCE
+})
+
+export const requestRobots = (source) => (dispatch) => {
 	dispatch({ type: REQUEST_ROBOTS_PENDING });
-	fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data}))
-		.catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error}));
+	if (source === SOURCE_API) {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data}))
+			.catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error}));
+	} else if (source === SOURCE_LOCAL) {
+		dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: robots});
+	}
 }
