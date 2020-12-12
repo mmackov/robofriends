46babaa32da9b84a80b74a345858a04234774555
Martin Mackov <martin.mackov@outlook.com>
2020-12-12 14:07:23 +0100

    Update labels when switching between robots sources

--- src/containers/App.js
+++ src/containers/App.js
@@ -48,7 +48,7 @@ class App extends Component {
 			<h1>Loading</h1> :
 			( 
 				<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
+					<h1 className='f1'>RoboFriends  -  {source}</h1>
 					<SearchBox searchChange={onSearchChange}/>
 					<SwitchSourceButton switchSource={onSwitchSource} label={source}/>
 					<Scroll>
